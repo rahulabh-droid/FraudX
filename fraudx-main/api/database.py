5e7786541b62d8553e2c80261b8ee133b1a58273
Rahul <pritysingh.5432@gmail.com>
2026-06-24 00:44:48 +0530

    Use environment variable for database URL

--- fraudx-main/api/database.py
+++ fraudx-main/api/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://fraudx_user:qFWf4hEF83evmIV6HjtYPOklw2B2ExoP@dpg-d8tcangk1i2s738i3t50-a.oregon-postgres.render.com/fraudx"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
